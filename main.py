b6d59cd7a50f42d425bb5c2f4e4f5c09abfe8f41
MIKE HAN <bravenw148@gmail.com>
2020-12-04 23:41:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+Fizzy_Buzz_Games
+
+for count in range(1, 101):
+  if count % 3 == 0 and count % 5 == 0:
+    print("Fizzybuzz") 
+  elif count % 3 == 0:
+    print("Fizzy")
+  elif count % 5 == 0:
+    print("Buzz")
+  else :
+    print(count)
+
+
